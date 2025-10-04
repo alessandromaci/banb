6d24d1f8a2e205973303b76a4eac79d57d5bbfc0
alerex.eth <alessandromaci96@gmail.com>
2025-10-04 22:49:44 +0200

    update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,13 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
